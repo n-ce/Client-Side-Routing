fcfadfed4bb2fc80d52e1c1fa12f54c9d7c025ea
Animesh <69345507+n-ce@users.noreply.github.com>
2023-04-13 12:45:49 +0530

    Update router.js

--- router.js
+++ router.js
@@ -28,7 +28,7 @@ function showPage(path) {
 // Update the browser history, then show page
 function route(path) {
 	// If the new page is equal to the current page, don't do anything
-	if (location.pathname.substring(20) === path) {
+	if (location.pathname.substring(19) === path) {
 		return;
 	}
 	window.history.pushState({}, '', new URL(path, origin));
@@ -50,4 +50,4 @@ for (const router of routers) {
 window.addEventListener('popstate', _ => showPage(location.pathname.substring(1)));
 
 // Show the current URL (to support direct linking aka deep links)
-showPage(location.pathname.substring(20));
+showPage(location.pathname.substring(19));
