6b3a331953ac304cac2f044d7145b531c1e553d7
n-ce <69345507+n-ce@users.noreply.github.com>
2023-04-13 06:41:31 +0000

    Github Pages Support

---
+++ router.js
@@ -0,0 +1,53 @@
+const routers = document.querySelectorAll('[data-router]');
+const content = document.querySelector('section');
+const pages = Array.from(document.querySelectorAll('[data-page]')); // Must convert to array to preserve elements
+location.origin += 'client-side-routing';
+// Display the appropriate page based on the URL path (does NOT modify browser history)
+function showPage(path) {
+	if (path === '' || path === 'index.html') {
+		path = 'home';
+	}
+
+	let match = false;
+	for (const page of pages) {
+		if (page.dataset.route === path) {
+			content.appendChild(page);
+			match = true;
+		}
+		else {
+			page.remove();
+		}
+	}
+	if (!match) {
+		// MUST redirect to a page where the web server serves an actual 404 error (i.e. not just 
+		// index.html again) otherwise this triggers an infinite loop
+		location.href = location.origin+'/errors/NotFound';
+	}
+}
+
+// Update the browser history, then show page
+function route(path) {
+	// If the new page is equal to the current page, don't do anything
+	if (location.pathname.substring(1) === path) {
+		return;
+	}
+	window.history.pushState({}, '', new URL(path, location.origin));
+
+	showPage(path);
+}
+
+// Set event handlers
+for (const router of routers) {
+	router.addEventListener('click', e => {
+		if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) {
+			return; // Use default behavior if any keyboard modifier is applied to the click
+		}
+
+		e.preventDefault();
+		route(e.target.dataset.route);
+	});
+}
+window.addEventListener('popstate', _ => showPage(window.location.pathname.substring(1)));
+
+// Show the current URL (to support direct linking aka deep links)
+showPage(location.pathname.substring(1));
